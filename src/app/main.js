52e207384973b5b7295e7463ff8e03e91cf1e086
marcosocon <oconmarcos97@gmail.com>
2017-09-18 16:27:14 -0300

    setup base project

---
+++ src/app/main.js
@@ -0,0 +1,7 @@
+angular
+  .module('app')
+  .component('app', {
+    templateUrl: 'app/main.html',
+    controller: 'MainController',
+    controllerAs: 'MainCtrl'
+  });
---
+++ src/index.js
@@ -0,0 +1,33 @@
+angular
+  .module('app', ['ui.router', 'chart.js'])
+  .controller('MainController', function () {
+    var vm = this;
+    vm.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    vm.series = ['Series A', 'Series B'];
+    vm.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+    vm.onClick = function (points, evt) {
+      console.log(points, evt);
+    };
+    vm.datasetOverride = [{yAxisID: 'y-axis-1'}, {yAxisID: 'y-axis-2'}];
+    vm.options = {
+      scales: {
+        yAxes: [
+          {
+            id: 'y-axis-1',
+            type: 'linear',
+            display: true,
+            position: 'left'
+          },
+          {
+            id: 'y-axis-2',
+            type: 'linear',
+            display: true,
+            position: 'right'
+          }
+        ]
+      }
+    };
+  });
